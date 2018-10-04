222f814f2b0f2bd494db57c83bdc1ef6612ea89c
Christian <cshinkle@iastate.edu>
2018-10-04 13:41:33 -0500

    Finished day 8 + 9

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './components/BusinessList/BusinessList';
+import './components/SearchBar/SearchBar'
+import SearchBar from './components/SearchBar/SearchBar';
+import BusinessList from './components/BusinessList/BusinessList';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList /> 
+      </div>      
     );
   }
 }
