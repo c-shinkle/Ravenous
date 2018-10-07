15130838fb5d0901871a8f1bcd87c730a8e99c92
Christian <cshinkle@iastate.edu>
2018-10-07 16:32:49 -0500

    Finished day 11

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -6,13 +6,34 @@ import './components/SearchBar/SearchBar'
 import SearchBar from './components/SearchBar/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList';
 
+let business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+let businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+];
+
 class App extends Component {
   render() {
     return (
       <div class="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList /> 
+        <BusinessList businesses={businesses}/> 
       </div>      
     );
   }
